9bdfe0ad86bc6955cd33bcd8aff9c1b79277aa96
PitchBlackNights <pitchblacknights@proton.me>
2025-12-11 11:33:34 -0600

    Finally publishing this

--- test.bat
+++ test.bat
@@ -2,14 +2,19 @@
 
 cargo build -p java-oxide-gen
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 pushd gen-test
 set "RUST_BACKTRACE=1"
+
 @REM ..\target\debug\java-oxide-gen.exe -vv generate
 ..\target\debug\java-oxide-gen.exe -vv generate >jog.txt 2>&1
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 cargo fmt -- --config-path ..\rustfmt.toml --config "max_width=200"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 cargo check >cargo-check-output.txt 2>&1
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 set "RUST_BACKTRACE="
 popd
